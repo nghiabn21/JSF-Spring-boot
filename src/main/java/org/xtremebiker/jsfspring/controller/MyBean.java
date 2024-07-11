21d0474e2be5121ced4db92bb267100ac6137aad
LPB\nghiahv3 <nghiahv3@lpbank.com.vn>
2024-07-11 05:43:41 -0500

    test

--- src/main/java/org/xtremebiker/jsfspring/controller/MyBean.java
+++ src/main/java/org/xtremebiker/jsfspring/controller/MyBean.java
@@ -2,17 +2,19 @@ package org.xtremebiker.jsfspring.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.primefaces.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.context.annotation.RequestScope;
 import org.xtremebiker.jsfspring.dto.StudentDto;
-import org.xtremebiker.jsfspring.entity.Student;
-import org.xtremebiker.jsfspring.repository.SkillRepo;
-import org.xtremebiker.jsfspring.service.StudentService;
+//import org.xtremebiker.jsfspring.entity.Student;
+//import org.xtremebiker.jsfspring.repository.SkillRepo;
+//import org.xtremebiker.jsfspring.service.StudentService;
 
 import javax.faces.annotation.ManagedProperty;
 import javax.faces.bean.ManagedBean;
@@ -26,33 +28,58 @@ import javax.validation.constraints.NotNull;
 @ManagedBean(name = "myBean", eager = true)
 @Validated
 public class MyBean {
-    @Autowired
-    private StudentService studentService;
+//    @Autowired
+//    private StudentService studentService;
 
     @Valid
     StudentDto student = new StudentDto();
 
-    public MyBean() {
-        System.out.println("Created!");
+    //
+//    public MyBean() {
+//        System.out.println("Created!");
+//    }
+//
+    public List<StudentDto> getAllStudent() {
+        List<StudentDto> studentDtos = new ArrayList<>();
+        studentDtos.add(new StudentDto(1, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(2, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(3, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(4, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(5, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(6, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(7, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(8, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        studentDtos.add(new StudentDto(9, "Nghĩa", "24", "Hà Nội", "Male", "Tester", LocalDateTime.now(), LocalDateTime.now(), new ArrayList<>()));
+        return studentDtos;
     }
 
-    public List<Student> getAllStudent() {
-        return studentService.getAllStudent();
-    }
-
-    public Boolean saveStudent() {
-        System.out.println("Student: " + student);
-        Student test = studentService.saveStudent(student);
-        return true;
-    }
-
-
+    //
+//    public Boolean saveStudent() {
+//        System.out.println("Student: " + student);
+//        Student test = studentService.saveStudent(student);
+//        return true;
+//    }
+//
+//
     public StudentDto getStudent() {
         return student;
     }
 
+    public String moveToPage2() {
+        return "update"; // outcome
+    }
+
     public String getDate() {
         return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+
+    public void closeDialog() {
+        RequestContext.getCurrentInstance().execute("PF('" + "dialogDetail" + "').hide();");
+    }
+
+    public void viewDetail(StudentDto studentDto) {
+        student = studentDto;
+        RequestContext.getCurrentInstance().execute("PF('" + "dialogDetail" + "').show();");
     }
 
 }
--- src/main/java/org/xtremebiker/jsfspring/dto/StudentDto.java
+++ src/main/java/org/xtremebiker/jsfspring/dto/StudentDto.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.xtremebiker.jsfspring.entity.Skill;
-
-import javax.persistence.*;
+//import org.xtremebiker.jsfspring.entity.Skill;
+//
+//import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @AllArgsConstructor
@@ -17,21 +19,25 @@ import java.util.List;
 @Setter
 public class StudentDto {
 
+    private Integer id;
 
     @NotNull(message = "Name is requied")
     private String name;
 
-    @Column(length = 128, nullable = false)
+    //    @Column(length = 128, nullable = false)
     private String age;
 
-    @Column(length = 128, nullable = false)
+    //    @Column(length = 128, nullable = false)
     private String address;
 
-    @Column(length = 128, nullable = false)
+    //    @Column(length = 128, nullable = false)
     private String gender;
 
-    @Column(length = 128, nullable = false)
+    //    @Column(length = 128, nullable = false)
     private String major;
+
+    private LocalDateTime fromDate;
+    private LocalDateTime toDate;
 
     private List<String> skills;
 }
